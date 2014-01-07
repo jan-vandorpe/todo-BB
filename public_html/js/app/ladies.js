b2c24d77d207352fd36da6adc973126355a71ac3
Jan Vandorpe <jan.vandorpe@vdab.be>
2014-01-07 10:51:32 +0100

    afgewerkt 1/14

---
+++ public_html/js/app/ladies.js
@@ -0,0 +1,17 @@
+/* 
+ hèt eigenlijke programma: ladies.js
+ */
+// ladies.js
+define([
+    'jquery', 
+    'underscore',
+    'backbone'
+], function($, _, Backbone){
+    return {
+        initialize: function(){
+            // you can use $, _ or Backbone here
+            console.log('ladies at the movies, men at home')
+        }
+    };
+});
+
---
+++ public_html/js/main.js
@@ -0,0 +1,30 @@
+/* 
+ RequireJS entry point voor Todo app
+ */
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: 'js/lib',
+    
+    paths: {
+        app             : '../app',
+        jquery          : 'jquery-2.0.3.min',
+        'underscore'    : 'underscore-amd/underscore-min', // AMD support
+        'backbone'      : 'backbone-amd/backbone-min', // AMD support,
+        'localstorage'  : 'backbone-amd/backbone.localstorage-min'// AMD support
+       }
+});
+console.log('main.js gestart')
+  
+require(['domReady!','app/todo'],
+    function   (doc, todo) {
+        todo.start(); //start de app
+ });
+
+//helper function
+function loadCss(url) {
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = url;
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
